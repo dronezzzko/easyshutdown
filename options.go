c1fb363c85b2bb2ec76ab95301ef2f9b9c2b512c
dronezzzko <andrey.gadyukov@gmail.com>
2023-02-04 19:02:41 +0200

    chore: options fix

--- options.go
+++ options.go
@@ -11,37 +11,37 @@ import (
 
 type Option func(sd *Shutdown)
 
-func (s *Shutdown) WithShutdownTimeout(d time.Duration) Option {
+func WithShutdownTimeout(d time.Duration) Option {
 	return func(sd *Shutdown) {
 		sd.shutdownTimeout = d
 	}
 }
 
-func (s *Shutdown) WithLogger(l *log.Logger) Option {
+func WithLogger(l *log.Logger) Option {
 	return func(sd *Shutdown) {
 		sd.logger = l
 	}
 }
 
-func (s *Shutdown) WithHTTPServer(srv *http.Server) Option {
+func WithHTTPServer(srv *http.Server) Option {
 	return func(sd *Shutdown) {
 		sd.services.httpServer = srv
 	}
 }
 
-func (s *Shutdown) WithHTTPSServer(srv *http.Server) Option {
+func WithHTTPSServer(srv *http.Server) Option {
 	return func(sd *Shutdown) {
 		sd.services.httpsServer = srv
 	}
 }
 
-func (s *Shutdown) WithGrpcServer(srv *grpc.Server) Option {
+func WithGrpcServer(srv *grpc.Server) Option {
 	return func(sd *Shutdown) {
 		sd.services.grpcServer = srv
 	}
 }
 
-func (s *Shutdown) WithTracerProvider(t *opentrace.TracerProvider) Option {
+func WithTracerProvider(t *opentrace.TracerProvider) Option {
 	return func(sd *Shutdown) {
 		sd.services.tracerProvider = t
 	}
